7064e9dc648b3b551491c5a74d0629754a3f59fe
Khalid Mim Muzahid <khalidmimm@gmail.com>
2025-04-11 16:48:59 +0600

    practice merge sort

--- Concepts/Algorithms/Majority Element/moore's voting algorithms/test.cpp
+++ Concepts/Algorithms/Majority Element/moore's voting algorithms/test.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 // Time Complexity: O(n)= n 
 int majorityElement(vector<int>& nums) {  
+    int n= nums.size();
     int answer;
     int ansCount=0;
 
@@ -21,12 +22,23 @@ int majorityElement(vector<int>& nums) {
 
     }
 
-    return answer;
+    // this is for, if there is no majority element then return -1
+    int count=0;
+    for(int val: nums){
+        if(val  == answer){
+            count++;
+        }
+    }
+    if(count > n/2){
+        return answer;
+    }
+    else{
+        return -1;
+    } 
 }
 
 int main(){
-    vector<int> vec=   {3,2,3};
-    // {2,2,1,1,1,2,2};
+    vector<int> vec= {2,2,1,1,1,2,2};
     int ans= majorityElement(vec);
     cout<<ans<< endl;
     return 0;
---
+++ Concepts/Algorithms/sorting/merge/test.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include  <vector> 
+using namespace std;
+
+void merge(vector<int> &arr, int start, int mid, int end){
+    int i=start, j=mid+1;
+    vector <int> tempArr;
+
+    while(i<j && i<=mid && j<=end){
+        if(arr[i]< arr[j]){
+            tempArr.push_back(arr[i]);
+            i++;
+        }
+        else{
+            tempArr.push_back(arr[j]);
+            j++;
+        }
+    }
+
+    while(i<= mid){ // if remaining element in left hub array, then element pushing to tempArr
+        tempArr.push_back(arr[i]);
+        i++;
+    }
+    while(j<= end){ // if remaining element in right hub array, then element pushing to tempArr
+        tempArr.push_back(arr[j]);
+        j++;
+    }
+
+    for(int i=0; i< tempArr.size(); i++){
+        arr[start+i] = tempArr[i];
+    }
+
+}
+
+// Time Complexity: O(n)= nlogn
+void mergeSort(vector<int> &arr, int start, int end){
+    if(start<end){
+        int mid = start+ (end-start)/2;
+        mergeSort(arr, start, mid); // for left hub
+        mergeSort(arr, mid+1, end); // for right hub
+
+        merge(arr, start, mid, end);
+    }
+
+    
+
+
+}
+
+void printVector(vector<int> arr, int n){
+    for(int i = 0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+    int n = 5;
+    vector<int> arr= {-2,1,-3,4,-1,2,1,-5,4};
+    int start=0, end=arr.size()-1;
+    mergeSort(arr,start, end);
+    printVector(arr, arr.size());
+
+    return 0;
+}
