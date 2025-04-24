d362df74967a1264dda34353e82c16309b2fa399
Khalid Mim Muzahid <khalidmimm@gmail.com>
2025-04-24 12:12:35 +0600

    solve leetcode problem of Next Permutationcd

--- leetcode/2025/April/24_ Next Permutation/solution/solution.cpp
+++ leetcode/2025/April/24_ Next Permutation/solution/solution.cpp
@@ -11,47 +11,54 @@ void printVector(vector<int>& vec){
 
 // TC => O(n), SC=> O(1)
 void nextPermutation(vector<int>& nums) { 
-    int n=nums.size(), breakPoint;
-    bool descendingOrder= true;
+    int n=nums.size();
+    int pivot=-1; // or breakpoint( a value or index that helps divide or guide a process in an algorithm.)
+
 
     // find is the already descending or not. if not then from right to left which index break the descending order from last of the array
+
+    // step 1
     for(int i = n-1; i>=1; i--){
         if(nums[i] > nums[i-1]){
-            breakPoint= i-1;
-            descendingOrder= false;
+            pivot= i-1;
             break;
             
         }
     }
 
-    if(!descendingOrder){
+    if(pivot != -1){
 
-        // we are try to place the value of nums[breakPoint] to the  index (breakpoint+1 to n-1) right side, so that  right side maintain the descending order. Thats why finding the targetIndex to place nums[breakPoint] 
+        // we are try to place the value of nums[pivot] to the  index (pivot+1 to n-1) right side, so that  right side maintain the descending order. Thats why finding the rightMost to place nums[pivot] 
 
         
-        // int targetIndex=breakPoint+1;
-        // for(int i = breakPoint+1; i<n; i++){
-        //     if(nums[i] > nums[breakPoint] && nums[i] <=nums[targetIndex]){
-        //         targetIndex=i;
+        //step 2
+
+        // int rightMost=pivot+1; // // targetIndex to place nums[pivot] value
+        // for(int i = pivot+1; i<n; i++){
+        //     if(nums[i] > nums[pivot] && nums[i] <=nums[rightMost]){
+        //         rightMost=i;
         //     }
         // }
 
         // OR,
 
-        int targetIndex=n-1;
-        for(int i = n-1; i>breakPoint; i--){
-            if(nums[i] > nums[breakPoint]){
-                targetIndex=i;
+        int rightMost=n-1; // targetIndex to place nums[pivot] value
+        for(int i = n-1; i>pivot; i--){
+            if(nums[i] > nums[pivot]){
+                rightMost=i;
                 break;
             }
         }
 
-        // we are placing the value of nums[breakPoint]
-        swap(nums[targetIndex], nums[breakPoint]);
+        // we are placing the value of nums[pivot]
+        swap(nums[rightMost], nums[pivot]);
 
+
+
+        // step 3
         // now right side is descending order. we just need to make it ascending ()
         // thats why we are just reversing the array
-        int start=breakPoint+1, end=n-1 ;
+        int start=pivot+1, end=n-1 ;
         while(start<=end){
             swap(nums[start++], nums[end--]);
         }
