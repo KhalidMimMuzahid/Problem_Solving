a91f34058f1b6d48d7550f608beae4e62fee5f5c
Khalid Mim Muzahid <khalidmimm@gmail.com>
2025-04-26 00:36:16 +0600

    practicing custom comparator in STL in cpp

--- Concepts/Language_Specific/CPP/STL/Algorithms/sort.cpp
+++ Concepts/Language_Specific/CPP/STL/Algorithms/sort.cpp
@@ -4,6 +4,20 @@
 using namespace std;
 
 
+//custom comparator
+bool comparator(pair<int,int>p1, pair<int,int>p2){
+    if(p1.second < p2.second)return true;
+    else return false;
+}
+
+bool comparator2(pair<int,int>p1, pair<int,int>p2){
+    if(p1.second < p2.second) return true;
+    else if(p1.second > p2.second) return false;
+    else if(p1.first < p2.first) return true;
+    else return false;
+}
+
+
 int main(){
     // start = memory address of first element 
     // end = memory address after the last element
@@ -52,6 +66,20 @@ int main(){
     cout<< endl;
 
     // sort by second value using custom comparator 
+    sort(vec.begin(), vec.end(), comparator); // sort in ascending (sort by second value)
+    cout<< "ascending by second: ";
+    for(auto val: vec){
+        cout<< "{" <<val.first<< ", "<< val.second<< "} ";
+    }
+    cout<< endl;
+
+    // sort by second value but if second value is equal then sort by first value using custom comparator 
+    sort(vec.begin(), vec.end(), comparator2); // sort in ascending (sort by second value)
+    cout<< "ascending by second then first: ";
+    for(auto val: vec){
+        cout<< "{" <<val.first<< ", "<< val.second<< "} ";
+    }
+    cout<< endl;
 
     return 0;
 }
