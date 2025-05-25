ea5226acc7df4c1a0c47ce70cc0fad93dbf6404c
Khalid Mim Muzahid <khalidmimm@gmail.com>
2025-05-25 12:43:45 +0600

    solved leetcode Flatten a Multilevel Doubly Linked List problem

---
+++ leetcode/2025/May/25_ Flatten a Multilevel Doubly Linked List/solution/solution.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+
+class Solution {
+public:
+    Node* helper(Node* current){
+        Node* nextNode= current->next;
+        Node* lastNode = current;
+        if(current != NULL && current->child != NULL){
+            current->next= current->child;
+            current->next->prev= current;
+            lastNode=helper(current->next);
+            if(nextNode != NULL){
+                lastNode->next= nextNode;
+                nextNode->prev= lastNode;
+            }
+            current->child= NULL; // remove child link
+        }
+        if(current != NULL && nextNode != NULL){
+            return helper(nextNode);
+        }
+        return lastNode;
+    }
+    Node* flatten(Node* head) {
+        if(head == NULL) return head;
+        Node* tail= helper(head);
+        tail->next= NULL;
+        return head;
+    }
+};
