87f9d8077d548dd1c04e124a844f8c71298b2450
Khalid Mim Muzahid <khalidmimm@gmail.com>
2025-05-25 22:10:24 +0600

    solved leetcode Swap Nodes in Pairs problem

---
+++ leetcode/2025/May/25_ Swap Nodes in Pairs/solution/solution.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+
+//  * Definition for singly-linked list.
+struct ListNode {
+public:
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* helper(ListNode* first, ListNode* second){
+        if(first==NULL || second==NULL) return first;
+
+        ListNode* nextFirstPair= second->next;
+        second->next= first;
+        if(nextFirstPair != NULL){
+            first->next= helper(nextFirstPair, nextFirstPair->next);
+        }
+        else{
+            first->next= NULL;
+        }
+        return second;
+    }
+
+    ListNode* swapPairs(ListNode* head) {
+        if(head== NULL)return NULL;
+        return helper(head, head->next);
+    }
+};
